792c50e38b38184782d1f61f705ebcd039a81405
Marcelo Souza <souzadeveloper@gmail.com>
2020-09-23 23:19:50 -0300

    Initial Commit

---
+++ SampleService/SampleService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace SampleService
+{
+    partial class SampleService : ServiceBase
+    {
+        public SampleService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.Interval = 60000;
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            var lista = new List<string>();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                lista.Add("Item " + i);
+            }
+
+            byte[] dataAsBytes = lista.SelectMany(s => System.Text.Encoding.UTF8.GetBytes(s + Environment.NewLine)).ToArray();
+        }
+    }
+}
